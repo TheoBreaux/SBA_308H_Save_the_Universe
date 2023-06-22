8ea7654717d9bd56be22586834bf361e27faf304
Theo Breaux <theobreaux@gmail.com>
2023-06-22 11:05:14 -0700

    completed logic

--- app.js
+++ app.js
@@ -28,112 +28,131 @@ function startGame() {
     playerName = "Player";
   }
   meetPlayerPrompt.style.display = "none";
-}
 
-//create my ship
-const myShip = {
-  name: "USS Assembly",
-  hull: 20,
-  firepower: 5,
-  accuracy: 0.7,
-  attackAlien(opponent) {
-    if (Math.random() < alienShips[0].accuracy) {
-      console.log("You have been hit!", this.hull);
-      this.hull -= alienShips[0].firepower;
-    } else if (alienShips[0].hull < 0) {
-      console.log("You win!", this.hull);
-      console.log("alien:", alienShips[0].hull);
-      alienShips.shift();
-    }
-    console.log(alienShips);
-  },
-};
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("attack-begins");
+  newDiv.innerHTML = `
+  <p class="challenge-text">
+      <span class="name">${playerName}</span>, <span class="prompt">the aliens have launched their attack! Click the ship to attack back!👽</span>
+  </p>
+  <img id="spaceship-image" src="./images/alien-ship1.png" alt="spaceship">
+`;
+  document.body.append(newDiv);
 
-//define function to generate random whole values for alien ship properties
-function randomWholeNumberGenerator(min, max) {
-  const range = max - min + 1;
-  const randomNum = Math.floor(Math.random() * range) + min;
-  return randomNum;
-}
+  //grab spaceship image to add event listener
+  const spaceshipImage = document.getElementById("spaceship-image");
 
-//define function to generate random float values for alien ship properties
-function randomFloatNumberGenerator(min, max) {
-  const range = max - min;
-  const randomNum = Math.random() * range + min;
-  const roundedNum = randomNum.toFixed(1);
-  return parseFloat(roundedNum);
+  // add eventlistener to spaceship Image
+  spaceshipImage.addEventListener("click");
 }
 
 //create ship class
 class Ship {
-  constructor(hull, firepower, accuracy, src) {
+  constructor(hull, firepower, accuracy) {
     this.hull = hull;
     this.firepower = firepower;
     this.accuracy = accuracy;
-    this.src = src;
   }
-  attack() {}
+  attack(target) {
+    if (Math.random() < this.accuracy) {
+      target.hull -= this.firepower;
+      console.log("You hit the alien ship!");
+
+      if (target.hull > 0) {
+        console.log(`The alien ship has ${target.hull}, remaining hull.`);
+        target.attack(this);
+      } else {
+        console.log("You destroyed the alien ship!");
+      }
+    } else {
+      console.log("Your attack missed");
+      target.attack(this);
+    }
+  }
 }
 
-//store alien ship objects in array
+//create my MyShip class
+class MyShip extends Ship {
+  constructor(hull, firepower, accuracy, name) {
+    super(hull, firepower, accuracy);
+    this.name = name;
+  }
+}
+
+// create AlienShip class
+class AlienShip extends Ship {
+  constructor() {
+    //store generated random values for ship properties (hull, firepower, accuracy)
+    const hull = Math.floor(Math.random() * 4) + 3;
+    const firepower = Math.floor(Math.random() * 3) + 2;
+    const accuracy = parseFloat((Math.random() * 0.2 + 0.6).toFixed(1));
+    super(hull, firepower, accuracy);
+  }
+  attack(target) {
+    if (Math.random() < this.accuracy) {
+      target.hull -= this.firepower;
+      console.log("You've been hit by the alien ship!");
+
+      if (target.hull > 0) {
+        console.log(`You have ${target.hull}, remaining hull.`);
+      } else {
+        console.log("You have been destroyed by the alien ship!");
+      }
+    } else {
+      console.log("The alien ship missed you!");
+      target.attack(this);
+    }
+  }
+}
+
+//create array to store alien ship objects
 const alienShips = [];
+//create my Myship instance for this battle round
+const UssAssembly = new MyShip(20, 5, 0.7, "USS Assembly");
 
-// creates the six alien ships and stores in array
-function battleShips() {
-  //create alien ships
+// create function to run battles
+function playGame() {
+  //create six Alienship instances with loop for this round
   for (let i = 0; i < 6; i++) {
-    const alienShip = new Ship(
-      randomWholeNumberGenerator(3, 6),
-      randomWholeNumberGenerator(2, 4),
-      randomFloatNumberGenerator(0.6, 0.8),
-      `./images/alien-ship${i}.png`
-    );
-    alienShips.push(alienShip);
+    // push randomly created AlienShips to array
+    alienShips.push(new AlienShip());
   }
-  return alienShips;
+  console.log("Alien ships have started attacking! Get them!");
+  // call function to attack alien ship, 0 is the first alien ship in the array
+  attackAliens(0);
 }
 
-console.log(battleShips());
+function attackAliens(index) {
+  const currentAlienShip = alienShips[index];
+  console.log("Attacking alien ship with hull", currentAlienShip.hull);
+  // i attack the first alien ship, 0 is the first alien ship in the array
+  UssAssembly.attack(currentAlienShip);
 
-function battle() {
-  // i attack first alien ship, if i survive attack again,
-  myShip.attackAlien();
+  if (currentAlienShip.hull <= 0) {
+    alienShips.shift();
+
+    if (alienShips.length > 0) {
+      let retreat = prompt(
+        "Alien ship has been destroyed. Attack again or retreat? (attack/retreat)"
+      );
+      if (retreat.toLowerCase() === "attack") {
+        attackAliens(index);
+      } else {
+        console.log("You retreated! Game Over");
+      }
+    } else {
+      console.log("Congratulations! You've won this battle");
+    }
+  } else {
+    let retreat = prompt(
+      "You have been hit. Attack again or retreat? (attack/retreat)"
+    );
+    if (retreat.toLowerCase() === "attack") {
+      attackAliens(index);
+    } else {
+      console.log("You retreated. Game Over");
+    }
+  }
 }
 
-battle();
-
-// const newDiv = document.createElement("div");
-// newDiv.classList.add("attack-begins");
-// newDiv.innerHTML = `
-//   <p class="challenge-text">
-//       <span class="name">${playerName}</span>, <span class="prompt">the aliens have launched their attack! Click the ship to attack back!👽</span>
-//   </p>
-//   <img id="spaceship-image" src="./images/alien-ship.png" alt="spaceship">
-// `;
-// document.body.append(newDiv);
-
-// //grab spaceship image to add event listener
-// const spaceshipImage = document.getElementById("spaceship-image");
-
-// // add eventlistener to spaceship Image
-// spaceshipImage.addEventListener("click", attackSpaceship);
-
-function attackSpaceship() {
-  console.log("I FEEL ATACKED!!!");
-  //alien ship survival = random number generated greater less than 0.5, alien attacks again
-  //else if random number is great than 0.5, i attack the alien ship
-  //if i destroy ship (ships score is 0 or less, i win)
-}
-
-const myVictory = () => {
-  //if i survive, i have an option to attack again or retreat
-  //if i retreat the game ends
-  //if i destroy all six alien ships I win
-  //i lose if i am destroyed
-};
-
-// there should be an i lose function
-const myDefeat = () => {
-  // i die, if number generated is less than 0.5
-  //if survuves attacks me again
-};
+// playGame();
